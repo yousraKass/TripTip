0bd1bc9c2066e06e37a44f762c979eeb3af9b997
yousraKass <kassousyousra@gmail.com>
2024-11-25 19:25:41 +0100

    define the fonts and colors to be used

---
+++ lib/style.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color main = Color(0x18C1C0);
+  static const Color second = Color(0x099094);
+  static const Color third = Color(0x2A6567);
+  static const Color fourth = Color(0x264653);
+  static const Color black = Color(0x000000);
+  static const Color white = Color(0xFFFFFF);
+}
+
+class FontFamily {
+  static const String regular = 'Poppins-Regular';
+  static const String medium = 'Poppins-Medium';
+  static const String bold = 'Poppins-Bold';
+}
